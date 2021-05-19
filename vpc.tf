452aabe764ce20cbbda859de5389621b8df40761
kerorofu <royce.code@gmail.com>
2021-05-19 16:59:42 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 2.5"
 
-    project_id   = var.project_id
+    project_id   = var.project
     network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
